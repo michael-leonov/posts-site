b7a738a618d86f29978f9dcbfb6a92e1bfad5c10
Michael <michael.leonov96@mail.ru>
2023-05-21 15:56:42 +0300

    add app routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
-import React from "react";
+import React from 'react';
+import AppRoutes from './routes';
 
-const App = () => {
-  return <div>Hello World</div>;
-};
+function App() {
+  return <AppRoutes />;
+}
 
 export default App;
---
+++ src/routes.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import Home from './pages/home';
+import About from './pages/about';
+import UserDetails from './pages/user-details';
+import NotFound from './pages/not-found';
+import { HOME_ROUTE, ABOUT_ROUTE, USER_DETAILS_ROUTE } from './utils/consts';
+
+function AppRoutes() {
+  return (
+    <Routes>
+      <Route path={HOME_ROUTE} element={<Home />} />
+      <Route path={ABOUT_ROUTE} element={<About />} />
+      <Route path={USER_DETAILS_ROUTE} element={<UserDetails />} />
+
+      <Route path='*' element={<NotFound />} />
+    </Routes>
+  );
+}
+
+export default AppRoutes;
