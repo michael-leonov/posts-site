94b59bea0eec156379a69e51b69dcc475802392c
Michael <michael.leonov96@mail.ru>
2023-05-22 22:05:23 +0300

    change user route

--- src/routes.jsx
+++ src/routes.jsx
@@ -4,14 +4,14 @@ import Home from './pages/home';
 import About from './pages/about';
 import UserDetails from './pages/user-details';
 import NotFound from './pages/not-found';
-import { HOME_ROUTE, ABOUT_ROUTE, USER_DETAILS_ROUTE } from './utils/consts';
+import { HOME_ROUTE, ABOUT_ROUTE, USER_ROUTE } from './utils/consts';
 
 function AppRoutes() {
   return (
     <Routes>
       <Route path={HOME_ROUTE} element={<Home />} />
       <Route path={ABOUT_ROUTE} element={<About />} />
-      <Route path={`${USER_DETAILS_ROUTE}/:id`} element={<UserDetails />} />
+      <Route path={`${USER_ROUTE}/:id`} element={<UserDetails />} />
 
       <Route path='*' element={<NotFound />} />
     </Routes>
