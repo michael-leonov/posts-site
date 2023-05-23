39c80a64521f52abbd4026c949ae03c663e7e3be
michael-leonov <michael.leonov96@mail.ru>
2023-05-23 14:45:31 +0300

    add burger menu

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import { Container } from 'react-bootstrap';
-
 import AppRoutes from './routes';
+import Header from './components/header';
 
 function App() {
   return (
     <Container>
+      <Header />
       <AppRoutes />
     </Container>
   );
