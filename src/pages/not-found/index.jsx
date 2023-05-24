c08a1947ddaff45ac6a9b66b26574d75e5f0e6f5
michael-leonov <michael.leonov96@mail.ru>
2023-05-24 15:42:28 +0300

    add styles 404 page

--- src/pages/not-found/index.jsx
+++ src/pages/not-found/index.jsx
@@ -1,7 +1,14 @@
-import React from "react";
+import React from 'react';
+import { Stack } from 'react-bootstrap';
+import './index.css';
 
 function NotFound() {
-  return <div>404 page</div>;
+  return (
+    <Stack className='align-items-center justify-content-center text-center px-4 flex-column flex-sm-row'>
+      <h1 className='tracking-tight pe-sm-4 me-sm-4 border-sm-right'>404</h1>
+      <h2 className='mt-2 fs-4 mt-sm-0'>Страница не найдена.</h2>
+    </Stack>
+  );
 }
 
 export default NotFound;
